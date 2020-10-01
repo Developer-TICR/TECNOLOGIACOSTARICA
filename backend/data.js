7adcf0b43b6e8928d02ad25df03e02dcad912608
Developer <developer@tecnologiacostarica.com>
2020-10-01 09:15:21 -0600

    F commit TICR

---
+++ backend/data.js
@@ -0,0 +1,114 @@
+export default {
+    products:[
+        {
+            _id: '1',
+            name: 'LENOVO S340',
+            image: '/images/LENOVO/LENOVO-S340.jpg',
+            price: 230,
+            brand: 'LENOVO',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '2',
+            name: 'TECLADO USB',
+            image: '/images/LENOVO/Teclado/teclado-lenovo-usb.png',
+            price: 25,
+            brand: 'LENOVO',
+            rating: 4.3,
+            numReviews: 12,
+            countInStock: 3
+        },
+        {
+            _id: '3',
+            name: 'TECLADO USB',
+            image: '/images/LENOVO/Teclado/teclado-lenovo-usb.png',
+            price: 25,
+            brand: 'LENOVO',
+            rating: 4.3,
+            numReviews: 12,
+            countInStock: 5
+        },
+        {
+            _id: '4',
+            name: 'LENOVO S340',
+            image: '/images/LENOVO/LENOVO-S340.jpg',
+            price: 230,
+            brand: 'LENOVO',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 1
+        },
+        {
+            _id: '5',
+            name: 'HEADSET H600',
+            image: '/images/LOGITECH/wireless-headset-h600.png',
+            price: 75,
+            brand: 'LOGITECH',
+            rating: 4.9,
+            numReviews: 7,
+            countInStock: 8
+        },
+        {
+            _id: '6',
+            name: 'HEADSET H600',
+            image: '/images/LOGITECH/wireless-headset-h600.png',
+            price: 75,
+            brand: 'LOGITECH',
+            rating: 4.9,
+            numReviews: 7,
+            countInStock: 6
+        },
+        {
+            _id: '7',
+            name: 'HEADSET H600',
+            image: '/images/LOGITECH/wireless-headset-h600.png',
+            price: 75,
+            brand: 'LOGITECH',
+            rating: 4.9,
+            numReviews: 7,
+            countInStock: 9
+        },
+        {
+            _id: '8',
+            name: 'LENOVO S340',
+            image: '/images/LENOVO/LENOVO-S340.jpg',
+            price: 230,
+            brand: 'LENOVO',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 3
+        },
+        {
+            _id: '9',
+            name: 'ERGONOMICO 613',
+            image: '/images/MICROSOFT/mouse-ergonomico.png',
+            price: 30,
+            brand: 'MICROSOFT',
+            rating: 4.8,
+            numReviews: 6,
+            countInStock: 0
+        },
+        {
+            _id: '10',
+            name: 'ERGONOMICO 613',
+            image: '/images/MICROSOFT/mouse-ergonomico.png',
+            price: 30,
+            brand: 'MICROSOFT',
+            rating: 4.8,
+            numReviews: 6,
+            countInStock: 1
+        },
+        {
+            _id: '11',
+            name: 'ERGONOMICO 613',
+            image: '/images/MICROSOFT/mouse-ergonomico.png',
+            price: 30,
+            brand: 'MICROSOFT',
+            rating: 4.8,
+            numReviews: 4,
+            countInStock: 5
+        }
+    ]
+}
